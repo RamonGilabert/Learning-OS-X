6578da18373c0d6d7a552bfe97784ebd121fee95
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 17:28:15 +0200

    Added some refactor

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -2,7 +2,8 @@ import Cocoa
 
 class MainWindowController: NSWindowController, NSUserNotificationCenterDelegate {
 
-    let textFieldPassword = NSTextField()
+    var textFieldPassword = NSTextField()
+    let viewModel = ViewModel()
 
     override var windowNibName: String? {
         return "MainWindowController"
@@ -14,20 +15,8 @@ class MainWindowController: NSWindowController, NSUserNotificationCenterDelegate
         self.window!.title = "Random passwords"
         self.window!.backgroundColor = NSColor(calibratedHue:0.98, saturation:0.59, brightness:0.93, alpha:1)
 
-        let buttonGenerate = NSButton(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, 25, 300, 40))
-        buttonGenerate.title = "Generate password"
-        buttonGenerate.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
-        buttonGenerate.target = self
-        buttonGenerate.action = "onGenerateButtonPressed"
-        self.window!.contentView.addSubview(buttonGenerate)
-
-        self.textFieldPassword.frame = NSMakeRect((self.window!.contentView.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50)
-        self.textFieldPassword.placeholderString = "Tap generate password"
-        self.textFieldPassword.font = NSFont(name: "AvenirNext-Regular", size: 22)
-        self.textFieldPassword.alignment = NSTextAlignment.CenterTextAlignment
-        self.textFieldPassword.editable = false
-        self.textFieldPassword.selectable = true
-        self.window!.contentView.addSubview(textFieldPassword)
+        self.viewModel.setButtonToGeneratePassword(self.window!.contentView as! NSView)
+        self.textFieldPassword = self.viewModel.setTextFieldInView(self.window!.contentView as! NSView)
     }
 
     func onGenerateButtonPressed() {
--- PasswordGenerator/PasswordGenerator/ViewModel.swift
+++ PasswordGenerator/PasswordGenerator/ViewModel.swift
@@ -1,13 +1,27 @@
-//
-//  ViewModel.swift
-//  PasswordGenerator
-//
-//  Created by Ramon Gilabert Llop on 4/26/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import Cocoa
 
 class ViewModel: NSObject {
 
+    func setButtonToGeneratePassword(view: NSView) {
+        let button = NSButton(frame: NSMakeRect((view.frame.width - 300)/2, 25, 300, 40))
+        button.title = "Generate password"
+        button.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        button.target = self
+        button.action = "onGenerateButtonPressed"
+
+        view.addSubview(button)
+    }
+
+    func setTextFieldInView(view: NSView) -> NSTextField {
+        let textField = NSTextField(NSMakeRect((view.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50))
+        textField.placeholderString = "Tap generate password"
+        textField.font = NSFont(name: "AvenirNext-Regular", size: 22)
+        textField.alignment = NSTextAlignment.CenterTextAlignment
+        textField.editable = false
+        textField.selectable = true
+
+        view.addSubview(textField)
+
+        return textField
+    }
 }
