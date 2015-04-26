39fc9622751066fa6f1f451ba1ead01b546470f6
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 18:05:42 +0200

    Second project updated

---
+++ RGBColor/RGBColor/MainWindowController.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        self.window!.title = "Color sliders"
+    }
+    
+}
