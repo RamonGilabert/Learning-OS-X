9eb65a0539b43759ff27b0d1ce39962477ef5299
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 17:34:06 +0200

    App done

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -12,32 +12,42 @@ class MainWindowController: NSWindowController, NSUserNotificationCenterDelegate
     override func windowDidLoad() {
         super.windowDidLoad()
 
+        NSUserNotificationCenter.defaultUserNotificationCenter().delegate = self
+
         self.window!.title = "Random passwords"
         self.window!.backgroundColor = NSColor(calibratedHue:0.98, saturation:0.59, brightness:0.93, alpha:1)
 
-        self.viewModel.setButtonToGeneratePassword(self.window!.contentView as! NSView)
+        self.viewModel.setButtonToGeneratePassword(self.window!.contentView as! NSView, windowController: self)
         self.textFieldPassword = self.viewModel.setTextFieldInView(self.window!.contentView as! NSView)
     }
 
     func onGenerateButtonPressed() {
         self.textFieldPassword.stringValue = generateRandomString(10)
 
-        let pasteboard = NSPasteboard.generalPasteboard()
-        pasteboard.clearContents()
-        pasteboard.writeObjects([self.textFieldPassword.stringValue])
-
-        let notification = NSUserNotification()
-        notification.title = "Password generator"
-        notification.informativeText = "Your password has been copied to your clipboard"
-        notification.soundName = NSUserNotificationDefaultSoundName
-
-        NSUserNotificationCenter.defaultUserNotificationCenter().delegate = self
-        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
+        updatePasteboard(self.textFieldPassword.stringValue)
+        presentNotification()
     }
 
     // MARK: Notification delegate
 
     func userNotificationCenter(center: NSUserNotificationCenter, shouldPresentNotification notification: NSUserNotification) -> Bool {
         return true
+    }
+
+    // MARK: Helper methods
+
+    func updatePasteboard(string: String) {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        pasteboard.clearContents()
+        pasteboard.writeObjects([string])
+    }
+
+    func presentNotification() {
+        let notification = NSUserNotification()
+        notification.title = "Password generator"
+        notification.informativeText = "Your password has been copied to your clipboard"
+        notification.soundName = NSUserNotificationDefaultSoundName
+
+        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
     }
 }
--- PasswordGenerator/PasswordGenerator/ViewModel.swift
+++ PasswordGenerator/PasswordGenerator/ViewModel.swift
@@ -2,18 +2,18 @@ import Cocoa
 
 class ViewModel: NSObject {
 
-    func setButtonToGeneratePassword(view: NSView) {
+    func setButtonToGeneratePassword(view: NSView, windowController: NSWindowController) {
         let button = NSButton(frame: NSMakeRect((view.frame.width - 300)/2, 25, 300, 40))
         button.title = "Generate password"
         button.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
-        button.target = self
+        button.target = windowController
         button.action = "onGenerateButtonPressed"
 
         view.addSubview(button)
     }
 
     func setTextFieldInView(view: NSView) -> NSTextField {
-        let textField = NSTextField(NSMakeRect((view.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50))
+        let textField = NSTextField(frame: NSMakeRect((view.frame.width - 300)/2, view.frame.height - 175, 300, 50))
         textField.placeholderString = "Tap generate password"
         textField.font = NSFont(name: "AvenirNext-Regular", size: 22)
         textField.alignment = NSTextAlignment.CenterTextAlignment
