456501d4d59739a4df19c62c6d66accc0fa861af
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 17:20:26 +0200

    Added support to show a notification

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-class MainWindowController: NSWindowController, NSTextDelegate {
+class MainWindowController: NSWindowController, NSUserNotificationCenterDelegate {
 
     let textFieldPassword = NSTextField()
 
@@ -32,5 +32,19 @@ class MainWindowController: NSWindowController, NSTextDelegate {
         let pasteboard = NSPasteboard.generalPasteboard()
         pasteboard.clearContents()
         pasteboard.writeObjects([self.textFieldPassword.stringValue])
+
+        let notification = NSUserNotification()
+        notification.title = "Password generator"
+        notification.informativeText = "Your password has been copied to your clipboard"
+        notification.soundName = NSUserNotificationDefaultSoundName
+
+        NSUserNotificationCenter.defaultUserNotificationCenter().delegate = self
+        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
+    }
+
+    // MARK: Notification delegate
+
+    func userNotificationCenter(center: NSUserNotificationCenter, shouldPresentNotification notification: NSUserNotification) -> Bool {
+        return true
     }
 }
