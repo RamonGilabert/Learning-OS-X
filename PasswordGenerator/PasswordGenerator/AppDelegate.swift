91b56cb08f6816a21d1fe4ab313335d58e8558f7
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 13:49:51 +0200

    Making it compile already

--- PasswordGenerator/PasswordGenerator/AppDelegate.swift
+++ PasswordGenerator/PasswordGenerator/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  PasswordGenerator
-//
-//  Created by Ramon Gilabert Llop on 4/26/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
@@ -13,15 +5,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
