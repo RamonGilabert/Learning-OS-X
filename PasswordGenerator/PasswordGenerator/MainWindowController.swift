d5a737cdaa689e9d9fab0338a44c0e2c0c3542ea
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 14:18:06 +0200

    Added the button finally

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -4,8 +4,13 @@ class MainWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-        self.window!.backgroundColor = NSColor.redColor()
 
-        
+        self.window!.title = "Random passwords"
+        self.window!.backgroundColor = NSColor(calibratedHue:0.98, saturation:0.59, brightness:0.93, alpha:1)
+
+        let buttonGenerate = NSButton(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, 40, 300, 50))
+        buttonGenerate.title = "Generate password"
+        buttonGenerate.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        self.window!.contentView.addSubview(buttonGenerate)
     }
 }
