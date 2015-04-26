d6389ea2a6ed1d59bbe135d480fe9b6cf783e040
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 14:39:18 +0200

    Changing text when you tap the button

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -2,6 +2,8 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
 
+    let textFieldPassword = NSTextField()
+
     override func windowDidLoad() {
         super.windowDidLoad()
 
@@ -11,14 +13,20 @@ class MainWindowController: NSWindowController {
         let buttonGenerate = NSButton(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, 25, 300, 40))
         buttonGenerate.title = "Generate password"
         buttonGenerate.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        buttonGenerate.target = self
+        buttonGenerate.action = "onGenerateButtonPressed"
         self.window!.contentView.addSubview(buttonGenerate)
 
-        let textFieldPassword = NSTextField(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50))
-        textFieldPassword.placeholderString = "Tap generate password"
-        textFieldPassword.font = NSFont(name: "AvenirNext-Regular", size: 22)
-        textFieldPassword.alignment = NSTextAlignment.CenterTextAlignment
-        textFieldPassword.editable = false
-        textFieldPassword.selectable = true
+        self.textFieldPassword.frame = NSMakeRect((self.window!.contentView.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50)
+        self.textFieldPassword.placeholderString = "Tap generate password"
+        self.textFieldPassword.font = NSFont(name: "AvenirNext-Regular", size: 22)
+        self.textFieldPassword.alignment = NSTextAlignment.CenterTextAlignment
+        self.textFieldPassword.editable = false
+        self.textFieldPassword.selectable = true
         self.window!.contentView.addSubview(textFieldPassword)
+    }
+
+    func onGenerateButtonPressed() {
+        self.textFieldPassword.stringValue = "Button pressed"
     }
 }
