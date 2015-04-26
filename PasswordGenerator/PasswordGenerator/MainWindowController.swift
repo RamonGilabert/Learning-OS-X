4c86f451386b61cf91b3ef6a8c85527b832963ee
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 14:25:54 +0200

    Added the code for the textField

--- PasswordGenerator/PasswordGenerator/MainWindowController.swift
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -8,9 +8,12 @@ class MainWindowController: NSWindowController {
         self.window!.title = "Random passwords"
         self.window!.backgroundColor = NSColor(calibratedHue:0.98, saturation:0.59, brightness:0.93, alpha:1)
 
-        let buttonGenerate = NSButton(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, 40, 300, 50))
+        let buttonGenerate = NSButton(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, 25, 300, 40))
         buttonGenerate.title = "Generate password"
         buttonGenerate.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
         self.window!.contentView.addSubview(buttonGenerate)
+
+        let textFieldPassword = NSTextField(frame: NSMakeRect((self.window!.contentView.frame.width - 300)/2, self.window!.contentView.frame.height - 175, 300, 50))
+        self.window!.contentView.addSubview(textFieldPassword)
     }
 }
