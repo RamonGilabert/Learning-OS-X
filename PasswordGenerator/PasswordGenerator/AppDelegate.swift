d4239ae1f4ea8014c9965b141c250e95beae2dd8
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-26 13:15:59 +0200

    Added the first project, password generator

---
+++ PasswordGenerator/PasswordGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PasswordGenerator
+//
+//  Created by Ramon Gilabert Llop on 4/26/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
