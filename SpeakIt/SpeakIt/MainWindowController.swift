06dfc4d8fea83597b8081df522e5449937b14c33
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-27 19:56:25 +0200

    Added code to handle actions

--- SpeakIt/SpeakIt/MainWindowController.swift
+++ SpeakIt/SpeakIt/MainWindowController.swift
@@ -14,11 +14,25 @@ class MainWindowController: NSWindowController {
         let speakItButton = NSButton(frame: NSMakeRect(self.window!.contentView.frame.width - 82.5, self.textField.frame.origin.y - 37.5, 75, 20))
         speakItButton.title = "Speak it"
         speakItButton.bezelStyle = NSBezelStyle.RoundedBezelStyle
+        speakItButton.target = self
+        speakItButton.action = "onSpeakItButtonPressed"
         self.window!.contentView.addSubview(speakItButton)
 
         let stopItButton = NSButton(frame: NSMakeRect(self.window!.contentView.frame.width - 155, self.textField.frame.origin.y - 37.5, 75, 20))
         stopItButton.title = "Stop it"
         stopItButton.bezelStyle = NSBezelStyle.RoundedBezelStyle
+        stopItButton.target = self
+        stopItButton.action = "onStopButtonPressed"
         self.window!.contentView.addSubview(stopItButton)
+    }
+
+    // MARK: Action handlers
+
+    func onSpeakItButtonPressed() {
+
+    }
+
+    func onStopItButtonPressed() {
+
     }
 }
