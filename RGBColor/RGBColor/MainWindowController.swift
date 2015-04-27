a97b0137426c3399eb21ca4a4854fb27a10a194e
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-27 11:04:54 +0200

    Playing with labels

--- RGBColor/RGBColor/MainWindowController.swift
+++ RGBColor/RGBColor/MainWindowController.swift
@@ -21,10 +21,32 @@ class MainWindowController: NSWindowController {
         self.redSlider.maxValue = 1.0
         self.window!.contentView.addSubview(self.redSlider)
 
+        let redLabel = NSTextField(frame: NSMakeRect(self.redSlider.frame.origin.x - 15, self.redSlider.frame.origin.y + 8, 18, 18))
+        redLabel.selectable = false
+        redLabel.editable = false
+        redLabel.bezeled = false
+        redLabel.bordered = false
+        redLabel.drawsBackground = false
+        redLabel.stringValue = "R"
+        redLabel.font = NSFont(name: "AvenirNext-Regular", size: 16)
+        redLabel.sizeToFit()
+        self.window!.contentView.addSubview(redLabel)
+
         self.greenSlider.frame = NSMakeRect(self.window!.contentView.frame.width - 175, self.window!.contentView.frame.height - 100, 150, 50)
         self.greenSlider.minValue = 0.0
         self.greenSlider.maxValue = 1.0
         self.window!.contentView.addSubview(self.greenSlider)
+
+        let redLabel = NSTextField(frame: NSMakeRect(self.redSlider.frame.origin.x - 15, self.redSlider.frame.origin.y + 8, 18, 18))
+        redLabel.selectable = false
+        redLabel.editable = false
+        redLabel.bezeled = false
+        redLabel.bordered = false
+        redLabel.drawsBackground = false
+        redLabel.stringValue = "R"
+        redLabel.font = NSFont(name: "AvenirNext-Regular", size: 16)
+        redLabel.sizeToFit()
+        self.window!.contentView.addSubview(redLabel)
 
         self.blueSlider.frame = NSMakeRect(self.window!.contentView.frame.width - 175, self.window!.contentView.frame.height - 200, 150, 50)
         self.blueSlider.minValue = 0.0
