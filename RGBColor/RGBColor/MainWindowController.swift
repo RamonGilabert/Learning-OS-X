06d7593eb69b75c9cae805c0e4bcfbb6534ff921
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-27 11:05:51 +0200

    Added the other two labels

--- RGBColor/RGBColor/MainWindowController.swift
+++ RGBColor/RGBColor/MainWindowController.swift
@@ -37,21 +37,32 @@ class MainWindowController: NSWindowController {
         self.greenSlider.maxValue = 1.0
         self.window!.contentView.addSubview(self.greenSlider)
 
-        let redLabel = NSTextField(frame: NSMakeRect(self.redSlider.frame.origin.x - 15, self.redSlider.frame.origin.y + 8, 18, 18))
-        redLabel.selectable = false
-        redLabel.editable = false
-        redLabel.bezeled = false
-        redLabel.bordered = false
-        redLabel.drawsBackground = false
-        redLabel.stringValue = "R"
-        redLabel.font = NSFont(name: "AvenirNext-Regular", size: 16)
-        redLabel.sizeToFit()
-        self.window!.contentView.addSubview(redLabel)
+        let greenLabel = NSTextField(frame: NSMakeRect(self.greenSlider.frame.origin.x - 15, self.greenSlider.frame.origin.y + 8, 18, 18))
+        greenLabel.selectable = false
+        greenLabel.editable = false
+        greenLabel.bezeled = false
+        greenLabel.bordered = false
+        greenLabel.drawsBackground = false
+        greenLabel.stringValue = "G"
+        greenLabel.font = NSFont(name: "AvenirNext-Regular", size: 16)
+        greenLabel.sizeToFit()
+        self.window!.contentView.addSubview(greenLabel)
 
         self.blueSlider.frame = NSMakeRect(self.window!.contentView.frame.width - 175, self.window!.contentView.frame.height - 200, 150, 50)
         self.blueSlider.minValue = 0.0
         self.blueSlider.maxValue = 1.0
         self.window!.contentView.addSubview(self.blueSlider)
+
+        let blueLabel = NSTextField(frame: NSMakeRect(self.blueSlider.frame.origin.x - 15, self.blueSlider.frame.origin.y + 8, 18, 18))
+        blueLabel.selectable = false
+        blueLabel.editable = false
+        blueLabel.bezeled = false
+        blueLabel.bordered = false
+        blueLabel.drawsBackground = false
+        blueLabel.stringValue = "B"
+        blueLabel.font = NSFont(name: "AvenirNext-Regular", size: 16)
+        blueLabel.sizeToFit()
+        self.window!.contentView.addSubview(blueLabel)
     }
     
 }
