670568be0f0fb80439dd7257219021c55e24f5d3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-28 12:22:43 +0200

    Added first styling

--- ToDoIt/ToDoIt/MainWindowController.swift
+++ ToDoIt/ToDoIt/MainWindowController.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-class MainWindowController: NSWindowController, NSWindowDelegate {
+class MainWindowController: NSWindowController, NSWindowDelegate, NSTableViewDelegate, NSTableViewDataSource {
 
     let tableView = NSTableView()
     let textFieldToDo = NSTextField()
@@ -9,13 +9,41 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
     override func windowDidLoad() {
         super.windowDidLoad()
 
+        self.window!.contentView.addSubview(self.textFieldToDo)
+        self.window!.contentView.addSubview(self.tableView)
+        self.window!.contentView.addSubview(self.addButton)
+
+        self.textFieldToDo.placeholderString = "Enter your task here, then tap the add button..."
+
+        self.addButton.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        self.addButton.target = self
+        self.addButton.action = "onAddButtonPressed"
+
         self.window!.title = "Just doing it"
         self.window!.delegate = self
+    }
+
+    // MARK: Action handlers
+
+    func onAddButtonPressed() {
+
+    }
+
+    // MARK: NSTableView delegate methods
+
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return 0
+    }
+
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        return nil
     }
 
     // MARK: NSWindow delegate methods
 
     func windowDidResize(notification: NSNotification) {
-        
+        self.textFieldToDo.frame = NSMakeRect(15, self.window!.contentView.frame.height - 45, self.window!.frame.width - 120, 30)
+        self.tableView.frame = NSMakeRect(15, 15, self.window!.frame.width - 30, self.window!.frame.height - 95)
+        self.addButton.frame = NSMakeRect(self.window!.frame.width - 90, self.window!.contentView.frame.height - 48, 75, 35)
     }
 }
