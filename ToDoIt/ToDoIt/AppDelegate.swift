909dc1c8926fd61deced9863ad5909e3a82411ad
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-28 12:04:25 +0200

    Added other code!

--- ToDoIt/ToDoIt/AppDelegate.swift
+++ ToDoIt/ToDoIt/AppDelegate.swift
@@ -1,27 +1,14 @@
-//
-//  AppDelegate.swift
-//  ToDoIt
-//
-//  Created by Ramon Gilabert Llop on 4/28/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    var mainWindowController: MainWindowController?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let mainWindowController = MainWindowController(windowNibName: "MainWindowController")
+        mainWindowController.showWindow(self)
+        self.mainWindowController = mainWindowController
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
