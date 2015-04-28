c591597de1042645f8eaa9198f8e7124fe151fc3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-28 19:50:44 +0200

    Power button is working now

--- Thermostat/Thermostat/MainWindowController.swift
+++ Thermostat/Thermostat/MainWindowController.swift
@@ -5,8 +5,10 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
     let temperatureSlider = NSSlider()
     let warmerButton = NSButton()
     let coolerButton = NSButton()
+    let powerButton = NSButton()
     let textFieldTemperature = NSTextField()
     var temperature = 68
+    var enableThermostat = true
 
     // MARK: View lifecycle
 
@@ -20,6 +22,7 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
         self.window!.contentView.addSubview(self.textFieldTemperature)
         self.window!.contentView.addSubview(self.warmerButton)
         self.window!.contentView.addSubview(self.coolerButton)
+        self.window!.contentView.addSubview(self.powerButton)
 
         self.temperatureSlider.minValue = 0
         self.temperatureSlider.maxValue = 100
@@ -40,18 +43,26 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
         self.coolerButton.target = self
         self.coolerButton.action = "onCoolerButtonPressed"
 
+        self.powerButton.bezelStyle = NSBezelStyle.RoundedBezelStyle
+        self.powerButton.title = "Power"
+        self.powerButton.setButtonType(NSButtonType.OnOffButton)
+
         self.temperatureSlider.bind("value", toObject: self, withKeyPath: "temperature", options: nil)
+        self.temperatureSlider.bind("enabled", toObject: self, withKeyPath: "enableThermostat", options: nil)
         self.textFieldTemperature.bind("value", toObject: self, withKeyPath: "temperature", options: nil)
+        self.warmerButton.bind("enabled", toObject: self, withKeyPath: "enableThermostat", options: nil)
+        self.coolerButton.bind("enabled", toObject: self, withKeyPath: "enableThermostat", options: nil)
+        self.powerButton.bind("value", toObject: self, withKeyPath: "enableThermostat", options: nil)
     }
 
     // MARK: Action handlers
 
     func onWarmerButtonPressed() {
-
+        setValue(temperature + 1, forKey: "temperature")
     }
 
     func onCoolerButtonPressed() {
-
+        setValue(temperature - 1, forKey: "temperature")
     }
 
     // MARK: NSWindowDelegate methods
@@ -61,5 +72,6 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
         self.textFieldTemperature.frame = NSMakeRect((self.window!.contentView.frame.width - 50)/2, (self.window!.contentView.frame.height - 30)/2, 30, 30)
         self.warmerButton.frame = NSMakeRect(self.window!.frame.width/2, self.window!.frame.height/2, self.window!.frame.width/2.5, self.window!.frame.height/2)
         self.coolerButton.frame = NSMakeRect(self.window!.frame.width/2, 0, self.window!.frame.width/2.5, self.window!.frame.height/2)
+        self.powerButton.frame = NSMakeRect(self.window!.contentView.frame.width - 100, 15, 75, 30)
     }
 }
