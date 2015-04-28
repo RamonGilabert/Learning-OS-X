26c18f2c831de99317edd3c1410259bd5192635d
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-28 12:05:52 +0200

    Added properties and basic methods

--- ToDoIt/ToDoIt/MainWindowController.swift
+++ ToDoIt/ToDoIt/MainWindowController.swift
@@ -1,9 +1,21 @@
 import Cocoa
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSWindowDelegate {
+
+    let tableView = NSTableView()
+    let textFieldToDo = NSTextField()
+    let addButton = NSButton()
 
     override func windowDidLoad() {
         super.windowDidLoad()
 
+        self.window!.title = "Just doing it"
+        self.window!.delegate = self
+    }
+
+    // MARK: NSWindow delegate methods
+
+    func windowDidResize(notification: NSNotification) {
+        
     }
 }
