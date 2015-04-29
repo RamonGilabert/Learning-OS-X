fe61c485c60d9c8023177870f234bf45f52a89fa
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-29 09:48:34 +0200

    Get the window programmatically and start to work with subviews

--- RaiseMan/RaiseMan/AppDelegate.swift
+++ RaiseMan/RaiseMan/AppDelegate.swift
@@ -4,7 +4,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+
     }
 }
 
--- RaiseMan/RaiseMan/Document.swift
+++ RaiseMan/RaiseMan/Document.swift
@@ -1,6 +1,9 @@
 import Cocoa
 
-class Document: NSDocument {
+class Document: NSDocument, NSWindowDelegate {
+
+    let tableView = NSTableView()
+    var documentWindow = NSWindow()
 
     override init() {
         super.init()
@@ -8,6 +11,12 @@ class Document: NSDocument {
 
     override func windowControllerDidLoadNib(aController: NSWindowController) {
         super.windowControllerDidLoadNib(aController)
+        let windowController = self.windowControllers[0] as! NSWindowController
+        self.documentWindow = windowController.window!
+        self.documentWindow.delegate = self
+
+        self.documentWindow.contentView.addSubview(self.tableView)
+
     }
 
     override class func autosavesInPlace() -> Bool {
@@ -24,5 +33,11 @@ class Document: NSDocument {
 
     override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
         return false
+    }
+
+    // MARK: NSWindow delegate methods
+
+    func windowDidResize(notification: NSNotification) {
+        self.tableView.frame = NSMakeRect(15, 15, self.documentWindow.contentView.frame.width/1.5, self.documentWindow.contentView.frame.height - 30)
     }
 }
