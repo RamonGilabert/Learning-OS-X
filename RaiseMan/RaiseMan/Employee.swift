4e32366950515ac1e78a7326770ba0aeb73de720
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-29 20:03:51 +0200

    Added a coder for the items to save

--- RaiseMan/RaiseMan/Employee.swift
+++ RaiseMan/RaiseMan/Employee.swift
@@ -1,7 +1,25 @@
 import Cocoa
 
-class Employee: NSObject {
+class Employee: NSObject, NSCoding {
 
     var name: String? = "New employee"
     var raise: NSNumber? = 0.05
+
+    func encodeWithCoder(aCoder: NSCoder) {
+        if let name = self.name {
+            aCoder.encodeObject(name, forKey: "name")
+        }
+
+        aCoder.encodeFloat(self.raise!.floatValue, forKey: "raise")
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObjectForKey("name") as? String
+        self.raise = aDecoder.decodeFloatForKey("raise")
+        super.init()
+    }
+
+    override init() {
+        super.init()
+    }
 }
