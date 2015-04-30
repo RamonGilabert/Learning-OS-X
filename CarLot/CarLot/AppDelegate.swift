22eba74d912974c7ba70c325c8a1f0afdd5e70c3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-30 10:33:34 +0200

    Added the new project, CarLot to the Table

---
+++ CarLot/CarLot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CarLot
+//
+//  Created by Ramon Gilabert Llop on 4/30/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CarLot/CarLot/Document.swift
@@ -0,0 +1,33 @@
+//
+//  Document.swift
+//  CarLot
+//
+//  Created by Ramon Gilabert Llop on 4/30/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override func windowControllerDidLoadNib(aController: NSWindowController) {
+        super.windowControllerDidLoadNib(aController)
+        // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override var windowNibName: String? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return "Document"
+    }
+
+}
