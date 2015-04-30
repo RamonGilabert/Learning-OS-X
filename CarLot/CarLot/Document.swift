54dc0ab366eb13a1073503985705a5fe7ce76cd9
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-30 11:20:38 +0200

    Playing with the Layout

--- CarLot/CarLot/Document.swift
+++ CarLot/CarLot/Document.swift
@@ -1,13 +1,13 @@
 import Cocoa
 
-class Document: NSPersistentDocument {
+class Document: NSPersistentDocument, NSWindowDelegate {
 
     let tableView = NSTableView()
     let addButton = NSButton()
     let removeButton = NSButton()
     let datePicker = NSDatePicker()
     let imageViewCar = NSImageView()
-    var documentWindow = NSWindow()
+    var documentView = NSView()
 
     override init() {
         super.init()
@@ -18,12 +18,36 @@ class Document: NSPersistentDocument {
         self.addWindowController(NSWindowController())
 
         (self.windowControllers.first as! NSWindowController).window = window
-        self.documentWindow = window!
+        self.documentView = window!.contentView as! NSView
+        window?.delegate = self
 
-        
+        self.documentView.addSubview(self.tableView)
+        self.documentView.addSubview(self.addButton)
+        self.documentView.addSubview(self.removeButton)
+
+        layoutFrameOfViews()
+
+        self.addButton.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        self.addButton.title = "Add"
+        self.removeButton.bezelStyle = NSBezelStyle.RegularSquareBezelStyle
+        self.removeButton.title = "Remove"
     }
 
     override class func autosavesInPlace() -> Bool {
         return true
+    }
+
+    // MARK: NSWindow delegate methods
+
+    func windowDidResize(notification: NSNotification) {
+        layoutFrameOfViews()
+    }
+
+    // MARK: Helper methods
+
+    func layoutFrameOfViews() {
+        self.tableView.frame = NSMakeRect(15, (self.documentView.frame.height * 0.6) - 15, self.documentView.frame.width - 30, (self.documentView.frame.height * 0.4))
+        self.addButton.frame = NSMakeRect((self.documentView.frame.width * 0.867) - 15, self.documentView.frame.height - self.tableView.frame.height - 55, self.documentView.frame.width/7.5, 25)
+        self.removeButton.frame = NSMakeRect(self.addButton.frame.origin.x - self.documentView.frame.width/7.5 - 15, self.documentView.frame.height - self.tableView.frame.height - 55, self.documentView.frame.width/7.5, 25)
     }
 }
