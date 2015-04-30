48b18ba2e4eb9bcf6ed0bc1ec82f0b2904416512
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-30 11:01:00 +0200

    Basic layout done

--- CarLot/CarLot/AppDelegate.swift
+++ CarLot/CarLot/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  CarLot
-//
-//  Created by Ramon Gilabert Llop on 4/30/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
@@ -14,13 +6,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var mainWindowController = Document()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let window = NSWindow(contentRect: NSMakeRect(100, 100, 200, 200), styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
+        let window = NSWindow(contentRect: NSMakeRect(100, 100, 575, 575), styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
         window.title = "New Window"
         window.opaque = false
         window.center()
         window.movableByWindowBackground = true
         window.makeKeyAndOrderFront(true)
-
+        window.minSize = CGSizeMake(575, 575)
         self.mainWindowController = Document(window: window)
     }
 }
--- CarLot/CarLot/Document.swift
+++ CarLot/CarLot/Document.swift
@@ -2,6 +2,11 @@ import Cocoa
 
 class Document: NSPersistentDocument {
 
+    let tableView = NSTableView()
+    let addButton = NSButton()
+    let removeButton = NSButton()
+    let datePicker = NSDatePicker()
+    let imageViewCar = NSImageView()
     var documentWindow = NSWindow()
 
     override init() {
@@ -10,10 +15,12 @@ class Document: NSPersistentDocument {
 
     init(window: NSWindow?) {
         super.init()
-        let windowController = NSWindowController()
-        self.addWindowController(windowController)
+        self.addWindowController(NSWindowController())
+
         (self.windowControllers.first as! NSWindowController).window = window
         self.documentWindow = window!
+
+        
     }
 
     override class func autosavesInPlace() -> Bool {
